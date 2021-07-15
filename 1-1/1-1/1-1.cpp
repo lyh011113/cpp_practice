25528da5942329e888889b527d7a777727a32579
YohanLee <john11113@naver.com>
2021-07-15 17:03:25 +0900

    1-1, 1-2

---
+++ 1-1/1-1/1-1.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+
+int main_1() {
+
+	std::cout << "Hello, World!!" << std::endl;
+
+	return 0;
+}
---
+++ 1-1/1-1/1-2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+int main(){
+
+	std::cout << "hi" << std::endl//std::endl -> /n¿ªÇÒ
+	<< "my name is "
+	<< "Psi" << std::endl;
+
+	std::cout << "hi" << " my name is " << "psi" << std::endl;
+	
+	
+	return 0;
+
+
+}
